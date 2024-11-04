b3c17bd6a904a6f197ad7caee1dedbe22cea084e
Gladwin Joseph <gladwinj@RPTECH.CO>
2024-11-04 17:51:45 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+
+app.use(cors({
+  origin: 'https://rma-frontend.onrender.com',
+}));
 app.use(express.json());
 const port= process.env.PORT || 5000
 
